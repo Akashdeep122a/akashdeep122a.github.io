3d1b238cc673ae1d4ef00b02830cafd7d06ec6aa
Akashdeep <akashdeep1k@gmail.com>
2024-12-06 19:15:33 +0530

    Improvements regarding the button

--- scripts/gui-manager.js
+++ scripts/gui-manager.js
@@ -1,11 +1,8 @@
 // Theme manager for managing theme
-function set_Relaxing_Theme(){
-    document.body.style.backgroundImage = 'url("https://pageno3.wordpress.com/wp-content/uploads/2024/05/download2818291131391897398642731.jpg")';
-}
 
-function set_Default_Theme(){
-    document.body.style.backgroundImage = 'linear-gradient(to bottom right, #ff7eb3, #ef5f77)';
-    document.body.style.backgroundImage = 'url("https://pageno3.wordpress.com/wp-content/uploads/2024/05/download2818291131391897398642731.jpg")';
+
+function Toggle_theme(){
+    document.body.classList.toggle("backgroundClass");
 }
 
 
